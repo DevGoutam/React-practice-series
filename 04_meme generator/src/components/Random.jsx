4703050421c97bedf2ee1f574ca3d49e8a3a1d8b
Dev goutam <Devgoutam805@gmail.com>
2026-01-11 22:30:59 +0530

    somethings have changed....

--- 04_meme generator/src/components/Random.jsx
+++ 04_meme generator/src/components/Random.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+const API_KEY = import.meta.env .REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
+
+  const [gif, setGif]= useState('')
+  
+  async function fetchData() {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+   const output = await axios.get(url);
+   console.log(output);
+    
+  }
+
+  useEffect (()=>{
+    fetchData();
+
+  },[])
 
     function clickHandler(){
         console.log("clicked")
@@ -8,7 +24,7 @@ const Random = () => {
   return (
     <div className='w-1/2 h-[450px] bg-green-500  rounded-xl border-2  border-white flex flex-col items-center mt-[15px] gap-y-6'>
       <h1 className='text-2xl underline uppercase font-bold'>a Random Gif</h1>
-      {/* <img src={gif} width={450}/> */}
+      <img src={gif} width={450}/>
       <button className='w-10/12 bg-green-100 text-lg py-2 rounded-lg '  onClick={clickHandler}>Generate</button>
     </div>
   )
